68cbfb63435f22b03a9ead71c7d8f64c17363cc7
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-11 17:49:21 +0100

    aula 11/oct
    var
    comentarios
    print
    input
    f str

---
+++ 0789 - Fundamentos de linguagem JAVA/demo1/main.py
@@ -0,0 +1,56 @@
+"""
+
+int - num inteiro
+str - texto 
+    " "
+    ''
+    """ """
+    ''' '''
+    
+float / double - num decimal
+bool - T / F
+
+"""
+
+nome = 0
+print(nome)
+
+
+nome = "João"
+print(nome)
+
+
+nome_aluno = "Carlos"
+
+nome2 = 0
+
+
+
+
+_nome = 10
+print(_nome)
+
+var_bool = True
+
+
+
+print(var_bool)
+
+
+
+my_name = bool(input("Qual o seu nome: ")) # lê um input e "converte" para bool
+
+"""
+comentario multi linha 
+"""
+
+
+# comentario linha
+
+my_name2 = input("Qual o seu nome: ")
+
+# "ola my_name2"
+print("ola " + my_name2)
+print("ola", my_name2)
+print("ola", my_name2, "em 2023") # str formatada
+print(f"ola {my_name2} em 2023") # str formatada
