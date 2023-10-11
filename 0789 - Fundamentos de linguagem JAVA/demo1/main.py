b94d6805980974a208ca6b3e05da6bd9d9182e7f
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-10-11 18:10:31 +0100

    operaçõies

--- 0789 - Fundamentos de linguagem JAVA/demo1/main.py
+++ 0789 - Fundamentos de linguagem JAVA/demo1/main.py
@@ -54,3 +54,51 @@ print("ola " + my_name2)
 print("ola", my_name2)
 print("ola", my_name2, "em 2023") # str formatada
 print(f"ola {my_name2} em 2023") # str formatada
+
+
+
+
+print("-" * 10, "Operações", "-" * 10)
+
+"""
+
++ <- att 1 + att2
+- <- att 1 - att2
+/ <- att 1 / att2
+* <- att 1 * att2
+
+
+"""
+
+
+soma1 = 10 + 10
+print(soma1)
+
+soma2 = 10 + 10.6
+print(soma2)
+
+
+# soma3 = 10 + "Joao" # neste contexto não e possivel
+# print(soma3)
+
+
+soma4 = "Joao" + "Carlos"
+print(soma4)
+
+soma5 = 10 + True
+print(soma5)
+
+soma6 = 10 + False
+print(soma6)
+
+# soma7 = "10" - "10"
+# print(soma7)
+
+#  " "1023" "
+num_as_str = "o valor é \"1023\"  "
+print(num_as_str)
+
+
+
+media = 10
+print(f"o joao foi aluno da \"ATEC\" com media de {media}")
