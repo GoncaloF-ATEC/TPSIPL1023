05c6e6eb8366ffa4983e45eb510924127dcbc4bd
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-11-08 18:04:45 +0000

    set e dict

--- 0789 - Fundamentos de linguagem JAVA/Semana4/Colections/main.py
+++ 0789 - Fundamentos de linguagem JAVA/Semana4/Colections/main.py
@@ -87,12 +87,124 @@ print("-" * 10)
 for elm in arr:
     print(elm)
 
-print("-" * 10)
-
+print("-" * 10, "Set", "-" * 10)
 
 #set
 
-#dicionarios
+mySet = {"Polvilho doce", "Polvilho azedo", "leite", "sal", "oleo", "queijo"}
+mySet2 = {"Polvilho azedo", "leite", "sal", "oleo", "agua", "açúcar"}
+
+
+print(mySet)
+# {'sal', 'leite', 'queijo', 'Polvilho doce', 'oleo', 'Polvilho azedo'}
+# {'leite', 'oleo', 'Polvilho doce', 'queijo', 'Polvilho azedo', 'sal'}
+
+#print(mySet[0])
+print("-" * 10)
+for elm in mySet:
+    print(elm)
+
+print("-" * 10)
+
+print("sal" in mySet)
+print("agua" in mySet)
+
+print("-" * 10)
+
+mySet.add("agua")
+
+print(mySet)
+print("agua" in mySet)
+
+print("-" * 10)
+
+mySet.remove("agua") # se agua nao existir gera um erro
+print("agua" in mySet)
+mySet.discard("agua")  # se agua nao existir não gera um erro
+
+print("-" * 10)
+print("-" * 10)
+mySet.add("agua")
+mySet.add("agua")
+mySet.add("agua")
+
+
+# https://media.geeksforgeeks.org/wp-content/uploads/20220110094813/download-660x416.png
+print(mySet)
+print(mySet2)
+print("-" * 3)
+print(mySet.union(mySet2))
+print("-" * 3)
+print(mySet.symmetric_difference(mySet2))
+
+print("-" * 3)
+print(mySet.intersection(mySet2))
+
+print("-" * 3)
+
+mySet = {"Polvilho doce", "Polvilho azedo", "leite", "sal", "oleo", "queijo"}
+mySet2 = {"Polvilho azedo", "leite", "sal", "oleo", "agua", "açúcar"}
+
+
+print(mySet.difference(mySet2))
+print(mySet2.difference(mySet))
+
+print("-" * 3)
+
+#Dicionarios
+print("-" * 10, "Dicionarios", "-" * 10)
+
+myDict = {"key1": "Valor2",
+          "key2": "Valor2",
+          "key3": "Valor2"}
+
+print(myDict)
+
+print(myDict["key1"])
+
+print(myDict.get("key1"))
+
+print("-" * 3)
+
+print(myDict.keys())
+print(myDict.values())
+
+print("-" * 3)
+
+myDict["new Key"] = "new Val"
+print(myDict)
+
+
+myDict["new Key"] = "novo Valor"
+print(myDict)
+
+myDict["key1"] = "Foo"
+print(myDict)
+
+print("-" * 3)
+
+"""
+myDict.pop("key1") # a chave tem de existir
+print(myDict)
+
+del myDict["key2"] # a chave tem de existir
+print(myDict)
+"""
+
+"""
+del myDict
+print(myDict) # remove o dict todo
+"""
+print("-" * 3)
+
+print("-" * 3)
+
+for elm in myDict.keys():  # for elm in myDict.keys():
+    print(elm) #print das chaves
+print("-" * 2)
+for elm in myDict.values():
+    print(elm) #print dos valroes
+
 
 
 #funcs
