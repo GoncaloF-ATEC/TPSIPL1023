848694869247801f070f393d9159e7e90cf8ccae
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-05-28 21:17:18 +0100

    opts

--- 5424 - Acesso móvel a sistemas de informação/opts/opts/main.swift
+++ 5424 - Acesso móvel a sistemas de informação/opts/opts/main.swift
@@ -59,3 +59,131 @@ print("---------")
 
 // opts
 
+// Int ≠ Int?  <- tipos de dados dif
+
+
+var idade:Int? // -> nil
+var idade2:Int // -> -----
+
+//print(idade)
+
+idade = 10
+
+idade = nil
+
+
+//aceder ao valor unwrapping
+print("-----unwrapping ->  ! ----")
+
+
+// !
+
+
+
+print(Int("10000")!)
+
+print("-----unwrapping ->  ??  ----")
+// ??
+
+
+
+print(Int("10000") ?? 0)
+print(Int("10_000") ?? 0)
+
+// if let
+print("-----unwrapping ->  if let  ----")
+
+var f:String? = "Var com valor"
+
+if let myF = f{
+    print(myF)
+    
+}else{
+    print("Sem valor")
+}
+
+
+if let f { // <=> if let f = f {
+    print(f)
+    
+}else{
+    print("Sem valor")
+}
+
+
+print("-----unwrapping ->  if let -> v2  ----")
+var num: Int? = 5
+
+if let num, num % 2 == 0 {
+    print("\(num) existe e é par")
+    
+}else{
+    print("Sem valor ou é impar")
+}
+
+
+// gaurd -> falar dps de funcs
+
+print("-----unwrapping ->  gaurd  ----")
+
+func teste(nota:Int? = nil){
+    
+    guard let nota else{ // <=>  guard let nota = nota else{
+        print("nota sem valor")
+        return
+    }
+    
+    print(nota)
+    
+    print("codigo, codigo e mais codigo")
+    
+}
+
+
+teste(nota: nil)
+teste()
+teste(nota: 31)
+
+// Null -> None -> nil
+
+
+
+// funcs
+
+print("---- funcs ----")
+
+func info(nome:String, ano:Int) -> String {
+    
+    var msg = "nome: \(nome), ano: \(ano)"
+    
+    return msg
+}
+
+
+func info2(nome:String, ano:Int) -> String {
+    "nome: \(nome), ano: \(ano)" // <=> return "nome: \(nome), ano: \(ano)"
+}
+
+let resp = info(nome: "Gonçalo", ano: 2024)
+
+print(resp)
+
+
+
+func soma2(n1 valor1:Int, n2 valor2:Int) -> Int{
+    valor1 + valor2
+}
+
+let f3 = soma2(n1: 3, n2: 3)
+
+
+
+func soma(_ valor1:Int, com valor2:Int) -> Int{
+    valor1 + valor2
+}
+let f1 = soma(59, com: 2)
+
+
+
+
+
